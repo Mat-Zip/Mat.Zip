5ee1298868406dbf422c82dab7f95492ba3ca668
jae5419 <jae5419@naver.com>
2022-05-11 16:11:45 +0900

    모달창 컴포넌트화

---
+++ src/components/ModalComp.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="$store.getters.getShowModal" class="modal-route" @click.self="backSpace">
+    <div class="modal-content">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    backSpace() {
+      this.$router.push('../');
+      this.$store.commit('setShowModal', false);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.modal-route {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: black;
+  opacity: 0.5;
+}
+
+.modal-route>.modal-content {
+  width: 80%;
+  height: 70%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: white;
+}
+</style>
--- src/views/MyView.vue
+++ src/views/MyView.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="about">
     <web-card v-for="place in $store.getters.getLikedPlaces" :key="place.id" :place="place"></web-card>
+    <modal-comp/>
   </div>
 </template>
 
 <script>
 import WebCard from '../components/WebCard.vue'
+import ModalComp from '@/components/ModalComp.vue'
 
 export default {
   components: {
-    WebCard
+    WebCard, ModalComp
   }
 }
 </script>
