e8903a95d481fb3ad40b033b31bff890bf666100
jagyeong gu <plzkoiu0912@naver.com>
2022-05-25 09:24:54 +0900

    calendar 기능 merge

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <h1>{{ $route.params.id }}</h1>
+  <div class="detail">
+    <div class="img">
+      <img src="" alt="">
+    </div>
+    <div class="intro">
+      <h1>{{ $route.params.id }}</h1>
+      <h1>title 블라블라</h1>
+      <p>intro 블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라</p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.detail {
+  display: flex;
+}
+
+.img {
+  width: 50%;
+}
+</style>
