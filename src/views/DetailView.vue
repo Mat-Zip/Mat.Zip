c5c1e77e243087feb4969858f8ade2b31582fa9c
jagyeong gu <plzkoiu0912@naver.com>
2022-05-26 10:39:19 +0900

    refactor: DetailView 코드 정리, dummy.json에 category/title/intro 추가

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="detail">
     <swiper class="swiper" :options="swiperOption">
-      <swiper-slide v-for="(img, i) in $store.state.places[$route.params.id].img" :key="i">
-        <img :src="require(`@/assets/카페정보/${$store.state.places[$route.params.id].name}/${img}`)"/>
+      <swiper-slide v-for="(img, i) in place.img" :key="i">
+        <img :src="require(`@/assets/카페정보/${place.name}/${img}`)" />
       </swiper-slide>
       <div class="swiper-button-prev" slot="button-prev"></div>
       <div class="swiper-button-next" slot="button-next"></div>
     </swiper>
     <div class="intro">
-      <h1>{{ $route.params.id }}</h1>
-      <h1>title 블라블라</h1>
-      <p>intro 블라블라블라블라블라블라블라블라블라블라블라</p>
-      <button @click="cl">console.log</button>
+      <h1>{{ place.title }}</h1>
+      <p>{{ place.intro }}</p>
     </div>
   </div>
 </template>
@@ -24,6 +22,7 @@ export default {
   components: { swiper, swiperSlide },
   data() {
     return {
+      place: this.$store.state.places[this.$route.params.id],
       index: 0,
       swiperOption: {
         slidesPerView: 1,
@@ -35,11 +34,6 @@ export default {
         },
       },
     };
-  },
-  methods: {
-    cl() {
-      console.log(this.place[this.$route.params.id].img);
-    },
   }
 };
 </script>
@@ -54,11 +48,12 @@ export default {
   height: 109.3vh;
 }
 
-/* .swiper .swiper-slide {
+.swiper .swiper-slide {
+  display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-} */
+}
 
 .intro {
   width: 50%;
