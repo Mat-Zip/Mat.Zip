a710d9d9f026f5e35a034037f6bfd3fc64930185
junyong <djdj4275@naver.com>
2022-05-30 12:44:16 +0900

    design : 버튼, 인포창 수정

--- src/components/InfoMap.vue
+++ src/components/InfoMap.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="info_window">
-        <h1>{{ place.name }}</h1>
+        <img class="info-img" :src="require(`../assets/카페정보/${place.name}/${place.img[0]}`)">
+        <div class="info-text">
+            <h1>{{ place.name }}</h1> <br>
+            <p>{{ place.address }}</p>
+            <p>{{ place.telephone }}</p>
+            <p>{{ place.time }}</p>
+            <p>{{ place.signature }}</p>
+        </div>
     </div>
 </template>
 
@@ -16,11 +23,22 @@ export default {
 .info_window {
   position: absolute;
   overflow: hidden;
-  top: 10px;
-  left: 10px;
+  top: 0px;
+  left: 0px;
   width: 300px;
   height: 100%;
   z-index: 1;
   background-color: white;
+  border: 1px solid black;
+  font-family: 'Noto Sans KR', sans-serif;
+}
+.info-img {
+    object-fit: cover;
+    height: 300px;
+}
+.info-text {
+    position: absolute;
+    bottom: 50px;
+    left: 10px;
 }
 </style>
