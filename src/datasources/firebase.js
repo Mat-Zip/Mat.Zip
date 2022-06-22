6e28f2b886e2bef8ada809fe8edc9e8f98dc3739
우재윤 <iwytbbtss00@naver.com>
2022-06-22 09:26:16 +0900

    etc: env 해제

--- src/datasources/firebase.js
+++ src/datasources/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.VUE_APP_FIREBASE_KEY,
+  apiKey: "AIzaSyAHX8avkAPeKuUssntuR5vsGXn41TfwVzY",
   authDomain: "mat-zip-a7f69.firebaseapp.com",
   databaseURL: "https://mat-zip-a7f69-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
